9e01616139648b6bca81b5b227d38c71c44448f0
ibnuirfan <ibnuirfan09@gmail.com>
2021-01-31 17:42:03 +0800

    added .env

--- esystem/esystem/settings.py
+++ esystem/esystem/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 import os
 
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6q)s&(nez+it(m__q*58!6rh*@9ipomq_e#zcdc4tmipxn@hs3'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
